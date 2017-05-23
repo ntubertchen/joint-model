6f9371768d08d711fb1ba92bd280e652de2c1ca0
alas79923 <b02902019@ntu.edu.tw>
2017-05-23 10:06:56 +0800

    sap

--- Code/sap.py
+++ Code/sap.py
@@ -20,6 +20,9 @@ we will then handle 28 sequences of 28 steps for every sample.
 '''
 # python s_v.py --glove "../glove/glove.6B.200d.txt" --train_p "../Data/" --slot_l "../Data/slot_l.txt" --intent_l "../Data/intent_l.txt"
 
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--glove', help='glove path')
 parser.add_argument('--train_p', help='training data path')
@@ -38,7 +41,7 @@ Data = DataPrepare(path)
 t_data = DataPrepare(t_path)
 # Parameters
 learning_rate = 0.0005
-epoc = 5
+epoc = 10
 batch_size = 1
 display_step = 50
 
@@ -86,24 +89,56 @@ def SAP_BiRNN(x, weights, biases):
     with tf.variable_scope('SAP_rnn'):
         outputs, _, _ = rnn.static_bidirectional_rnn(lstm_fw_cell, lstm_bw_cell, x,dtype=tf.float32)
     # Linear activation, using rnn inner loop last output
-    return tf.matmul(outputs[-1], weights['SAP_out']) + biases['SAP_out']
+    pred = []
+    for i in range(S_n_sentences):
+        pred.append(tf.matmul(outputs[i], weights['SAP_out']) + biases['SAP_out'])
+    return pred
 
 SAP_pred = SAP_BiRNN(sap_x, weights, biases)
+SAP_pred = tf.reduce_mean(SAP_pred,0)
+_pred = tf.sigmoid(SAP_pred)
+def toone(logit):
+    for i in range(len(logit[0])):
+        for j in range(len(logit[0][i])):
+            if logit[0][i][j] >= 0.35:
+                logit[0][i][j] = 1
+            else:
+                logit[0][i][j] = 0
+    return logit[0]
 
-# else:
-#     pred = intent_BiRNN(x,weights,biases)
-#     pred_out = tf.argmax(pred,axis=1)
-#     loss = intent_loss(pred,y_intent)
-# Define loss and optimize
+def acc_sap(logit,label):
+    tp = 0
+    tn = 0
+    fp = 0
+    fn = 0
+    logit = toone(logit)
+    for i in range(len(logit)):
+        for j in range(len(logit[i])):
+            if logit[i][j] > 0.9 and label[i][j] > 0.9:
+                tp += 1
+            elif logit[i][j] < 0.5 and label[i][j] < 0.5:
+                tn += 1
+            elif logit[i][j] < 0.9 and label[i][j] > 0.9:
+                fn += 1
+            elif logit[i][j] > 0.5 and label[i][j] < 0.5:
+                fp += 1
+    _p = tp+fp
+    _r = tp+fn
+    if _p == 0:
+        p = float(tp)/1
+    else:
+        p = float(tp)/(tp+fp)
+    if _r == 0:
+        r = float(tp)/1
+    else:
+        r = float(tp)/(tp+fn)
+    return tp,fp,fn
 
-correct_pred = tf.equal(tf.argmax(SAP_pred,axis=1), tf.argmax(sap_y,axis=1))
-accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+#correct_pred = tf.equal(tf.argmax(SAP_pred,axis=1), tf.argmax(sap_y,axis=1))
+#accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
 sap_cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=sap_y,logits=SAP_pred))
 _SAP_optimizer = tf.train.AdamOptimizer(learning_rate=S_learning_rate).minimize(sap_cost)
-# Evaluate model
-#correct_pred = tf.equal(tf.argmax(pred,1), tf.argmax(y,1)) 
-#accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
 saver = tf.train.Saver()
 
@@ -112,7 +147,7 @@ init = tf.global_variables_initializer()
 
 if args.test == False:
     # Launch the graph
-    with tf.Session() as sess:
+    with tf.Session(config=config) as sess:
         sess.run(init)
         # Keep training until reach max iterations
         seq_in,seq_out,seq_int,seq_info,seq_rev = Data.get_all()
@@ -135,9 +170,28 @@ if args.test == False:
                 #print('b',summed_int.shape)
                 tags_con = np.concatenate((summed_slot,_i_nlu_out),axis=1)
                 _ = sess.run([_SAP_optimizer],feed_dict={sap_x:[tags_con],sap_y:[batch_intent[-1]]})
-
-                if i % 1000 == 0:
-                    print ("sap cost:",sess.run([sap_cost],feed_dict={sap_x:[tags_con],sap_y:[batch_intent[-1]]}))
+                if i % 3000 == 0 and i != 0:
+                    i1=i2=i3=0
+                    for i in range(len(t_in)):
+                        batch_x =t_in[i]
+                        batch_slot=t_out[i]
+                        batch_intent=t_int[i]
+                        batch_info = t_info[i]
+                        if batch_info[-1].strip() != "Guide":
+                            continue
+                        _s_nlu_out = batch_slot[3:-1]
+                        summed_slot = np.sum(_s_nlu_out,axis=1)
+                        _i_nlu_out = batch_intent[3:-1]
+                        tags_con = np.concatenate((summed_slot,_i_nlu_out),axis=1)
+                        sapp = sess.run([_pred],feed_dict={sap_x:[tags_con],sap_y:[batch_intent[-1]]})
+                        f1,f2,f3 = acc_sap(sapp,[batch_intent[-1]])
+                        i1 += f1
+                        i2 += f2
+                        i3 += f3
+                    _p = float(i1)/(i1+i2)
+                    _r = float(i1)/(i1+i3)
+                    print ("sap:recall:",_r,",precision:",_p,",f score:",2*_r*_p/(_r+_p))
+            
             step += 1
         save_path = saver.save(sess,"../model/sapmodel.ckpt")
         # print("Optimization Finished!")
@@ -146,12 +200,3 @@ else:
         ckpt = tf.train.get_checkpoint_state("../model")
         if ckpt and ckpt.model_checkpoint_path:
             saver.restore(sess,ckpt.model_checkpoint_path)
-        # test_x,test_y,origin_x = Data.get_test()
-        # slot_out = sess.run(pred,feed_dict={x: [test_x]})
-        # origin_x = origin_x.strip().split()
-        # out = []
-        # for z in range(len(slot_out)):
-        #     out.append(np.argmax(slot_out[z]))
-        # for z in range(len(origin_x)):
-        #     if Data.rev_slotdict[out[len(out)-1-z]] != 'O':
-        #         print (Data.rev_slotdict[out[len(out)-1-z]],origin_x[z])
