b9cebc006c32d356ad69fe6454d7be6a6dbff73c
james <b02902126@ntu.edu.tw>
2017-06-28 00:25:38 +0800

    reach the same f1 score

--- Code/slu.py
+++ Code/slu.py
@@ -11,19 +11,6 @@ from sklearn.preprocessing import Binarizer
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 
-FLAGS = flags.FLAGS
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--glove', help='glove path')
-parser.add_argument('--train_p', help='training data path')
-parser.add_argument('--test_p', help='testing data path')
-parser.add_argument('--test', action='store_true', help='test or train')
-parser.add_argument('--slot_l', help='slot tag path')
-parser.add_argument('--intent_l', help='intent path')
-parser.add_argument('--intent', action='store_false', help='intent training')
-parser.add_argument('--slot', action='store_false', help='slot training')
-args = parser.parse_args()
-
 def process_nl(batch_nl, batch_intent, max_seq_len, intent_pad_id, nl_pad_id, total_intent):
     train_tourist = list()
     train_guide = list()
@@ -125,7 +112,7 @@ def one_hot(idx, T):
 if __name__ == '__main__':
     sess = tf.Session(config=config)
     data = slu_data()
-    max_seq_len = 60
+    max_seq_len = 50
     total_intent = data.total_intent
     total_word = data.total_word
     model = slu_model(max_seq_len, total_intent)
@@ -133,33 +120,76 @@ if __name__ == '__main__':
     # read in the glove embedding matrix
     sess.run(model.init_embedding, feed_dict={model.read_embedding_matrix:data.embedding_matrix})
 
-    epoch = 25
+    epoch = 5
     use_intent = False # True: use intent tag as input, False: use nl as input
 
     # Train
     for cur_epoch in range(epoch):
-        # get the data
-        batch_nl, batch_intent = data.get_train_batch(256)
-        train_intent = None
-        train_nl = None
-        if use_intent == True:
-            train_tourist, train_guide, train_nl, train_target, tourist_len, guide_len, nl_len = process_intent(batch_nl, batch_intent, max_seq_len, total_intent-1, total_word-1, total_intent)
-        else:
-            train_tourist, train_guide, train_nl, train_target, tourist_len, guide_len, nl_len = process_nl(batch_nl, batch_intent, max_seq_len, total_intent-1, total_word-1, total_intent)
+        pred_outputs = list()
+        train_targets = list()
+        total_loss = 0.0
+        for _ in range(50):
+            # get the data
+            batch_nl, batch_intent = data.get_train_batch(256)
+            train_intent = None
+            train_nl = None
+            if use_intent == True:
+                train_tourist, train_guide, train_nl, train_target, tourist_len, guide_len, nl_len = process_intent(batch_nl, batch_intent, max_seq_len, total_intent-1, total_word-1, total_intent)
+            else:
+                train_tourist, train_guide, train_nl, train_target, tourist_len, guide_len, nl_len = process_nl(batch_nl, batch_intent, max_seq_len, total_intent-1, total_word-1, total_intent)
+            # add nl_indices to gather_nd of bi-rnn output
+            nl_indices = list()
+            for idx, indices in enumerate(nl_len):
+                nl_indices.append([idx, indices])
 
-        _, intent_output = sess.run([model.train_op, model.intent_output],
-                feed_dict={
-                    model.input_nl:train_nl,
-                    model.labels:train_target,
-                    model.nl_len:nl_len,
-                    })
-   
+            _, intent_output, loss = sess.run([model.train_op, model.intent_output, model.loss],
+                    feed_dict={
+                        model.input_nl:train_nl,
+                        model.labels:train_target,
+                        model.nl_indices:nl_indices
+                        })
+            total_loss += loss
+            for pred, label in zip(intent_output, train_target):
+                
+                pred_act = pred[:5] # first 5 is act
+                pred_attribute = pred[5:] # remaining is attribute
+                binary = Binarizer(threshold=0.5)
+                act_logit = one_hot(np.argmax(pred_act), 'act')
+                attribute_logit = binary.fit_transform([pred_attribute])
+                if np.sum(attribute_logit) == 0:
+                    attribute_logit = one_hot(np.argmax(pred_attribute), 'attribute')
+                label = binary.fit_transform([label])
+                pred_outputs = np.append(pred_outputs, np.append(act_logit, attribute_logit))
+                train_targets = np.append(train_targets, label)
         # calculate batch F1 score
+        print "f1 score is:", f1_score(pred_outputs, train_targets, average='binary')
+        print "loss is:", total_loss
+        print "cur_epoch is:", cur_epoch
+
+        # Test
+        test_nl, test_intent = data.get_test_batch()
+        if use_intent == True:
+            test_tourist, test_guide, test_nl, test_target, tourist_len, guide_len, nl_len = process_intent(test_nl, test_intent, max_seq_len, total_intent-1, total_word-1, total_intent)
+        else:
+            test_tourist, test_guide, test_nl, test_target, tourist_len, guide_len, nl_len = process_nl(test_nl, test_intent, max_seq_len, total_intent-1, total_word-1, total_intent)
+
+        # add nl_indices to gather_nd of bi-rnn output
+        nl_indices = list()
+        for idx, indices in enumerate(nl_len):
+            nl_indices.append([idx, indices])
+        test_output = sess.run(model.intent_output,
+                feed_dict={
+                    model.input_nl:test_nl,
+                    model.labels:test_target,
+                    model.nl_indices:nl_indices
+                    })
+
+        # calculate test F1 score
         pred_vec = np.array(list())
         label_vec = np.array(list())
-        for pred, label in zip(intent_output, train_target):
-            pred_act = pred[:5]
-            pred_attribute = pred[5:]
+        for pred, label in zip(test_output, test_target):
+            pred_act = pred[:5] # first 5 is act
+            pred_attribute = pred[5:] # remaining is attribute
             binary = Binarizer(threshold=0.5)
             act_logit = one_hot(np.argmax(pred_act), 'act')
             attribute_logit = binary.fit_transform([pred_attribute])
@@ -168,32 +198,5 @@ if __name__ == '__main__':
             label = binary.fit_transform([label])
             pred_vec = np.append(pred_vec, np.append(act_logit, attribute_logit))
             label_vec = np.append(label_vec, label)
-        print "f1 score is:", f1_score(pred_vec, label_vec, average='binary')
-        print "cur_epoch is:", cur_epoch
-
-    # Test
-    test_nl, test_intent = data.get_test_batch()
-    if use_intent == True:
-        test_tourist, test_guide, test_nl, test_target, tourist_len, guide_len, nl_len = process_intent(test_nl, test_intent, max_seq_len, total_intent-1, total_word-1, total_intent)
-    else:
-        test_tourist, test_guide, test_nl, test_target, tourist_len, guide_len, nl_len = process_nl(test_nl, test_intent, max_seq_len, total_intent-1, total_word-1, total_intent)
-
-    test_output = sess.run(model.intent_output,
-            feed_dict={
-                model.input_nl:test_nl,
-                model.nl_len:nl_len,
-                model.labels:test_target
-                })
-
-    # calculate test F1 score
-    pred_vec = np.array(list())
-    label_vec = np.array(list())
-    for pred, label in zip(test_output, test_target):
-        label_bin = Binarizer(threshold=0.5)
-        pred_bin = Binarizer(threshold=0.2)
-        logit = pred_bin.fit_transform([pred])
-        label = label_bin.fit_transform([label])
-        pred_vec = np.append(pred_vec, logit)
-        label_vec = np.append(label_vec, label)
-    print "test f1 score is:", f1_score(pred_vec, label_vec, average='binary')
+        print "test f1 score is:", f1_score(pred_vec, label_vec, average='binary')
     sess.close()
--- Code/slu_model.py
+++ Code/slu_model.py
@@ -25,44 +25,34 @@ class slu_model(object):
         self.embedding_matrix = tf.Variable(tf.truncated_normal([self.total_word, self.embedding_dim]), dtype=tf.float32, name="glove_embedding")
 
     def add_placeholders(self):
-        # used to indicate the actual length of sequence, we don't want padding to be calculated
-        self.nl_len = tf.placeholder(tf.int32, [None])
         # natural language input sequence, which is also the utterance we are going to predict(intents)
         self.input_nl = tf.placeholder(tf.int32, [None, self.max_seq_len])
         # pretrained word embedding matrix
         self.read_embedding_matrix = tf.placeholder(tf.float32, [self.total_word, self.embedding_dim])
         # correct label that used to calculate sigmoid cross entropy loss, should be [batch_size, intent_dim]
         self.labels = tf.placeholder(tf.float32, [None, self.intent_dim])
+        # used to extract the right time step from output of bi-rnn
+        self.nl_indices = tf.placeholder(tf.int32, [None, 2])
 
     def nl_biRNN(self):
+	
         with tf.variable_scope("nl"):
             inputs = tf.nn.embedding_lookup(self.embedding_matrix, self.input_nl) # [batch_size, self.max_seq_len, self.embedding_dim]
-            lstm_fw_cell = rnn.BasicLSTMCell(self.hidden_size)
-            lstm_bw_cell = rnn.BasicLSTMCell(self.hidden_size)
-            _, final_states = tf.nn.bidirectional_dynamic_rnn(lstm_fw_cell, lstm_bw_cell, inputs, sequence_length=self.nl_len, dtype=tf.float32)
-            final_fw = tf.concat(final_states[0], axis=1)
-            final_bw = tf.concat(final_states[1], axis=1)
-            outputs = tf.concat([final_fw, final_bw], axis=1) # concatenate forward and backward final states
+            lstm_fw_cell = rnn.BasicLSTMCell(self.hidden_size, forget_bias=1.0)
+            lstm_bw_cell = rnn.BasicLSTMCell(self.hidden_size, forget_bias=1.0)
+            outputs, _ = tf.nn.bidirectional_dynamic_rnn(lstm_fw_cell, lstm_bw_cell, inputs, dtype=tf.float32)
+            fw_outputs = tf.gather_nd(outputs[0], self.nl_indices)
+            bw_outputs = tf.gather_nd(outputs[1], self.nl_indices)
+            outputs = tf.concat([fw_outputs, bw_outputs], axis=1) # concatenate forward and backward final states
             return outputs
 
     def build_graph(self):
-        final_output = self.nl_biRNN()
-        self.intent_output = tf.layers.dense(inputs=final_output, units=self.intent_dim, activation=tf.nn.sigmoid)
+        outputs = self.nl_biRNN()
+        self.intent_output = tf.layers.dense(inputs=outputs, units=self.intent_dim, kernel_initializer=tf.random_normal_initializer, bias_initializer=tf.random_normal_initializer)
 
     def add_loss(self):
         self.loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=self.labels, logits=self.intent_output))
         
     def add_train_op(self):
         optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
-        gvs = optimizer.compute_gradients(self.loss)
-        # clip the gradients
-	def ClipIfNotNone(grad):
-            '''
-            if grad is None:
-                return grad
-            return tf.clip_by_value(grad, -1, 1)
-            '''
-            return grad
-        capped_gvs = [(ClipIfNotNone(grad), var) for grad, var in gvs]
-        optimizer.apply_gradients(capped_gvs)
         self.train_op = optimizer.minimize(self.loss)
