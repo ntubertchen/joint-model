893b06120743b085b7a09b017502b8ccc3a50dc4
james <b02902126@ntu.edu.tw>
2017-07-01 16:36:29 +0800

    finish mid loss, lack attention

--- Code/slu.py
+++ Code/slu.py
@@ -39,19 +39,23 @@ def process_nl(batch_nl, batch_intent, max_seq_len, intent_pad_id, nl_pad_id, to
     for i in batch_nl:
         temp_tourist_list = list()
         temp_guide_list = list()
+        temp_tourist_len = list()
+        temp_guide_len = list()
         history = i[:-1]
         hist_len = len(history) / 2 # tourist, guide
         # tourist part
         for nl in history[:hist_len]:
-            temp_tourist_list += nl
+            temp_tourist_list.append(nl+[nl_pad_id for _ in range(max_seq_len - len(nl))])
+            temp_tourist_len.append(len(nl))
         # guide part
         for nl in history[hist_len:]:
-            temp_guide_list += nl
-        # pad the sequence
-        train_tourist.append(temp_tourist_list+[nl_pad_id for _ in range(max_seq_len - len(temp_tourist_list))])
-        tourist_len.append(len(temp_tourist_list))
-        train_guide.append(temp_guide_list+[nl_pad_id for _ in range(max_seq_len - len(temp_guide_list))])
-        guide_len.append(len(temp_guide_list))
+            temp_guide_list.append(nl+[nl_pad_id for _ in range(max_seq_len - len(nl))])
+            temp_guide_len.append(len(nl))
+    
+        train_tourist.append(temp_tourist_list)
+        tourist_len.append(temp_tourist_len)
+        train_guide.append(temp_guide_list)
+        guide_len.append(temp_guide_len)
 
     for i in batch_intent:
         target_idx.append([i[-1][0]]+[attri for attri in i[-1][1]])
@@ -87,16 +91,13 @@ def process_intent(batch_nl, batch_intent, max_seq_len, intent_pad_id, nl_pad_id
         # tourist part
         for tup in history[:hist_len]:
             d = [tup[0]] + [attri for attri in tup[1]]
-            temp_tourist_list = np.concatenate(temp_tourist_list, one_hot(d, 'mix'))
+            temp_tourist_list.append(one_hot(d, 'mix'))
         # guide part
         for tup in history[hist_len:]:
             d = [tup[0]] + [attri for attri in tup[1]]
-            temp_guide_list = np.contenate(temp_guide_list, one_hot(d, 'mix'))
-        # pad the sequence
-        train_tourist.append(temp_tourist_list+[intent_pad_id for _ in range(max_seq_len - len(temp_tourist_list))])
-        tourist_len.append(len(temp_tourist_list))
-        train_guide.append(temp_guide_list+[intent_pad_id for _ in range(max_seq_len - len(temp_guide_list))])
-        guide_len.append(len(temp_guide_list))
+            temp_guide_list.append(one_hot(d, 'mix'))
+        train_guide.append(temp_guide_list)
+        train_tourist.append(temp_tourist_list)
         target_idx.append([i[-1][0]]+[attri for attri in i[-1][1]])
 
     for i in batch_nl:
@@ -115,9 +116,9 @@ def process_intent(batch_nl, batch_intent, max_seq_len, intent_pad_id, nl_pad_id
 
 if __name__ == '__main__':
     sess = tf.Session(config=config)
-    max_seq_len = 70
+    max_seq_len = 40
     epoch = 30
-    batch_size = 64
+    batch_size = 256
     use_attention = True
 
     data = slu_data()
@@ -136,38 +137,29 @@ if __name__ == '__main__':
         for _ in range(50):
             # get the data
             batch_nl, batch_intent = data.get_train_batch(batch_size)
-            train_intent = None
-            train_nl = None
-            train_tourist_intent, train_guide_intent, train_nl_intent, train_target_intent, tourist_len_intent, guide_len_intent, nl_len_intent = process_intent(batch_nl, batch_intent, max_seq_len, total_intent-1, total_word-1, total_intent)
-            train_tourist_nl, train_guide_nl, train_nl_nl, train_target_nl, tourist_len_nl, guide_len_nl, nl_len_nl = process_nl(batch_nl, batch_intent, max_seq_len, total_intent-1, total_word-1, total_intent)
+            train_tourist_intent, train_guide_intent, train_nl, train_target_intent, tourist_len_intent, guide_len_intent, nl_len = process_intent(batch_nl, batch_intent, max_seq_len, total_intent-1, total_word-1, total_intent)
+            train_tourist_nl, train_guide_nl, train_nl, train_target_nl, tourist_len_nl, guide_len_nl, nl_len = process_nl(batch_nl, batch_intent, max_seq_len, total_intent-1, total_word-1, total_intent)
+            assert train_target_intent == train_target_nl
             # add nl_indices to gather_nd of bi-rnn output
             nl_indices_intent = list()
             nl_indices_nl = list()
-            for idx, indices in enumerate(nl_len_intent):
+            for idx, indices in enumerate(nl_len):
                 nl_indices_intent.append([idx, indices])
-            for idx, indices in enumerate(nl_len_nl):
-                nl_indices_nl.append([idx, indices])
-
             _, intent_output, loss = sess.run([model.train_op, model.intent_output, model.loss],
                     feed_dict={
                         model.tourist_input_intent:train_tourist_intent,
                         model.guide_input_intent:train_guide_intent,
-                        model.input_nl_intent:train_nl_intent,
-                        model.tourist_len_intent:tourist_len_intent,
-                        model.guide_len_intent:guide_len_intent,
-                        model.labels_intent:train_target_intent,
-                        model.nl_len_intent:nl_len_intent,
+                        model.labels:train_target_intent,
                         model.tourist_input_nl:train_tourist_nl,
                         model.guide_input_nl:train_guide_nl,
-                        model.input_nl_nl:train_nl_nl,
+                        model.predict_nl:train_nl,
                         model.tourist_len_nl:tourist_len_nl,
                         model.guide_len_nl:guide_len_nl,
-                        model.labels_nl:train_target_nl,
-                        model.nl_len_nl:nl_len_nl,
+                        model.predict_nl_len:nl_len,
                         model.dropout_keep_prob:0.5
                         })
             total_loss += loss
-            for pred, label in zip(intent_output, train_target):
+            for pred, label in zip(intent_output, train_target_intent):
                 pred_act = pred[:5] # first 5 is act
                 pred_attribute = pred[5:] # remaining is attribute
                 binary = Binarizer(threshold=0.5)
@@ -184,30 +176,35 @@ if __name__ == '__main__':
         print "training loss:", total_loss
 
         # Test
-        test_nl, test_intent = data.get_test_batch()
-        test_tourist, test_guide, test_nl, test_target, tourist_len, guide_len, nl_len = process_intent(test_nl, test_intent, max_seq_len, total_intent-1, total_word-1, total_intent)
-        test_tourist, test_guide, test_nl, test_target, tourist_len, guide_len, nl_len = process_nl(test_nl, test_intent, max_seq_len, total_intent-1, total_word-1, total_intent)
+        test_batch_nl, test_batch_intent = data.get_test_batch()
+        test_tourist_intent, test_guide_intent, test_nl, test_target_intent, tourist_len_intent, guide_len_intent, nl_len = process_intent(test_batch_nl, test_batch_intent, max_seq_len, total_intent-1, total_word-1, total_intent)
+        test_tourist_nl, test_guide_nl, test_nl, test_target_nl, tourist_len_nl, guide_len_nl, nl_len = process_nl(test_batch_nl, test_batch_intent, max_seq_len, total_intent-1, total_word-1, total_intent)
+        assert test_target_intent == test_target_nl
         test_output = sess.run(model.intent_output,
                 feed_dict={
-                    model.tourist_input:test_tourist[:5000],
-                    model.guide_input:test_guide[:5000],
-                    model.input_nl:test_nl[:5000],
-                    model.tourist_len:tourist_len[:5000],
-                    model.guide_len:guide_len[:5000],
-                    model.nl_len:nl_len[:5000],
-                    model.labels:test_target[:5000],
+                    #model.tourist_input_intent:test_tourist_intent[:5000],
+                    #model.guide_input_intent:test_guide_intent[:5000],
+                    model.labels:test_target_intent[:5000],
+                    model.tourist_input_nl:test_tourist_nl[:5000],
+                    model.guide_input_nl:test_guide_nl[:5000],
+                    model.predict_nl:test_nl[:5000],
+                    model.tourist_len_nl:tourist_len_nl[:5000],
+                    model.guide_len_nl:guide_len_nl[:5000],
+                    model.predict_nl_len:nl_len[:5000],
                     model.dropout_keep_prob:1.0
                     })
         
         test_output = np.concatenate((test_output, sess.run(model.intent_output,
                 feed_dict={
-                    model.tourist_input:test_tourist[5000:],
-                    model.guide_input:test_guide[5000:],
-                    model.input_nl:test_nl[5000:],
-                    model.tourist_len:tourist_len[5000:],
-                    model.guide_len:guide_len[5000:],
-                    model.nl_len:nl_len[5000:],
-                    model.labels:test_target[5000:],
+                    #model.tourist_input_intent:test_tourist_intent[5000:],
+                    #model.guide_input_intent:test_guide_intent[5000:],
+                    model.labels:test_target_intent[5000:],
+                    model.tourist_input_nl:test_tourist_nl[5000:],
+                    model.guide_input_nl:test_guide_nl[5000:],
+                    model.predict_nl:test_nl[5000:],
+                    model.tourist_len_nl:tourist_len_nl[5000:],
+                    model.guide_len_nl:guide_len_nl[5000:],
+                    model.predict_nl_len:nl_len[5000:],
                     model.dropout_keep_prob:1.0
                     })), axis=0)
 
@@ -216,9 +213,9 @@ if __name__ == '__main__':
         test_talker = open('Data/test/talker', 'r')
         pred_vec = np.array(list())
         label_vec = np.array(list())
-        for pred, label, talker in zip(test_output, test_target, test_talker):
-            if talker.strip('\n') == 'Guide':
-                continue
+        for pred, label, talker in zip(test_output, test_target_intent, test_talker):
+            #if talker.strip('\n') == 'Tourist':
+            #    continue
             pred_act = pred[:5] # first 5 is act
             pred_attribute = pred[5:] # remaining is attribute
             binary = Binarizer(threshold=0.5)
--- Code/slu_model.py
+++ Code/slu_model.py
@@ -9,8 +9,9 @@ class slu_model(object):
         self.embedding_dim = 200 # read from glove
         self.total_word = 400002 # total word embedding vectors
         self.max_seq_len = max_seq_len
-        self.filter_sizes = [2,3,4]
-        self.filter_depth = 128
+        self.filter_sizes = [3,4,5]
+        self.filter_depth = 64
+        self.hist_len = 3
         self.use_attention = use_attention
         self.add_variables()
         self.add_placeholders()
@@ -28,32 +29,29 @@ class slu_model(object):
         self.embedding_matrix = tf.Variable(tf.truncated_normal([self.total_word, self.embedding_dim]), dtype=tf.float32, name="glove_embedding")
 
     def add_placeholders(self):
-        self.tourist_input_intent = tf.placeholder(tf.int32, [None, self.max_seq_len])
-        self.guide_input_intent = tf.placeholder(tf.int32, [None, self.max_seq_len])
-        self.tourist_len_intent = tf.placeholder(tf.int32, [None])
-        self.guide_len_intent = tf.placeholder(tf.int32, [None])
-        self.nl_len_intent = tf.placeholder(tf.int32, [None])
-        self.input_nl_intent = tf.placeholder(tf.int32, [None, self.max_seq_len])
+        self.tourist_input_intent = tf.placeholder(tf.float32, [None, self.hist_len, self.intent_dim])
+        self.guide_input_intent = tf.placeholder(tf.float32, [None, self.hist_len, self.intent_dim])
         self.read_embedding_matrix = tf.placeholder(tf.float32, [self.total_word, self.embedding_dim])
-        self.labels_intent = tf.placeholder(tf.float32, [None, self.intent_dim])
-        self.tourist_input_nl = tf.placeholder(tf.int32, [None, self.max_seq_len])
-        self.guide_input_nl = tf.placeholder(tf.int32, [None, self.max_seq_len])
-        self.tourist_len_nl = tf.placeholder(tf.int32, [None])
-        self.guide_len_nl = tf.placeholder(tf.int32, [None])
-        self.nl_len_nl = tf.placeholder(tf.int32, [None])
-        self.input_nl_nl = tf.placeholder(tf.int32, [None, self.max_seq_len])
-        self.read_embedding_matrix = tf.placeholder(tf.float32, [self.total_word, self.embedding_dim])
-        self.labels_intent = tf.placeholder(tf.float32, [None, self.intent_dim])
+        self.labels = tf.placeholder(tf.float32, [None, self.intent_dim])
+        self.tourist_input_nl = tf.placeholder(tf.int32, [None, self.hist_len, self.max_seq_len])
+        self.guide_input_nl = tf.placeholder(tf.int32, [None, self.hist_len, self.max_seq_len])
+        self.tourist_len_nl = tf.placeholder(tf.int32, [None, self.hist_len])
+        self.guide_len_nl = tf.placeholder(tf.int32, [None, self.hist_len])
+        self.predict_nl_len = tf.placeholder(tf.int32, [None])
+        self.predict_nl = tf.placeholder(tf.int32, [None, self.max_seq_len])
         self.dropout_keep_prob = tf.placeholder(tf.float32)
 
-    def hist_cnn(self, scope):
+    def hist_cnn(self, scope, idx):
         with tf.variable_scope(scope):
+            if idx != 0:
+                tf.get_variable_scope().reuse_variables()
             if scope == 'tourist':
-                inputs = tf.nn.embedding_lookup(self.intent_matrix, self.tourist_input)
-                seq_len = self.tourist_len
+                # tourist_input_nl should now have [batch_size, 3, max_seq_len]
+                tourist_input_nl = tf.unstack(self.tourist_input_nl, axis=1)[idx]
+                inputs = tf.nn.embedding_lookup(self.embedding_matrix, tourist_input_nl)
             elif scope == 'guide':
-                inputs = tf.nn.embedding_lookup(self.intent_matrix, self.guide_input)
-                seq_len = self.guide_len
+                guide_input_nl = tf.unstack(self.guide_input_nl, axis=1)[idx]
+                inputs = tf.nn.embedding_lookup(self.embedding_matrix, guide_input_nl)
             pooled_outputs = list()
             for idx, filter_size in enumerate(self.filter_sizes):
                 # convolution layer
@@ -65,7 +63,8 @@ class slu_model(object):
             num_filters_total = self.filter_depth * len(self.filter_sizes)
             h_pool_flat = tf.squeeze(tf.concat(pooled_outputs, axis=2), axis=1)
             h_drop = tf.nn.dropout(h_pool_flat, self.dropout_keep_prob)
-            return h_drop
+            dense_h = tf.layers.dense(inputs=h_drop, units=self.intent_dim, kernel_initializer=tf.random_normal_initializer, bias_initializer=tf.random_normal_initializer)
+            return dense_h
 
     def hist_biRNN(self, scope):
         with tf.variable_scope(scope):
@@ -86,53 +85,59 @@ class slu_model(object):
 
     def nl_biRNN(self, history_summary):
         with tf.variable_scope("nl"):
-            inputs = tf.nn.embedding_lookup(self.embedding_matrix, self.input_nl) # [batch_size, self.max_seq_len, self.embedding_dim]
+            inputs = tf.nn.embedding_lookup(self.embedding_matrix, self.predict_nl) # [batch_size, self.max_seq_len, self.embedding_dim]
             history_summary = tf.expand_dims(history_summary, axis=1)
             replicate_summary = tf.tile(history_summary, [1, self.max_seq_len, 1]) # [batch_size, self.max_seq_len, self.intent_dim]
             concat_input = tf.concat([inputs, replicate_summary], axis=2) # [batch_size, self.max_seq_len, self.intent_dim+self.embedding_dim]
             lstm_fw_cell = rnn.BasicLSTMCell(self.hidden_size)
             lstm_bw_cell = rnn.BasicLSTMCell(self.hidden_size)
-            _, final_states = tf.nn.bidirectional_dynamic_rnn(lstm_fw_cell, lstm_bw_cell, concat_input, sequence_length=self.nl_len, dtype=tf.float32)
+            _, final_states = tf.nn.bidirectional_dynamic_rnn(lstm_fw_cell, lstm_bw_cell, concat_input, sequence_length=self.predict_nl_len, dtype=tf.float32)
             final_fw = tf.concat(final_states[0], axis=1)
             final_bw = tf.concat(final_states[1], axis=1)
             outputs = tf.concat([final_fw, final_bw], axis=1) # concatenate forward and backward final states
             return outputs
 
-    def role_attention(self, tourist_rnn_hist, guide_rnn_hist):
-        tourist_cnn_hist = self.hist_cnn('tourist')
-        guide_cnn_hist = self.hist_cnn('guide')
+    def role_attention(self):
+        self.unstack_tourist_cnn_hist = list()
+        self.unstack_guide_cnn_hist = list()
+        for i in range(self.hist_len):
+            self.unstack_tourist_cnn_hist.append(self.hist_cnn('tourist', i))
+            self.unstack_guide_cnn_hist.append(self.hist_cnn('guide', i))
+        # TODO: naively concat all the history
+        tourist_cnn_hist = tf.sigmoid(tf.concat(self.unstack_tourist_cnn_hist, axis=1))
+        guide_cnn_hist = tf.sigmoid(tf.concat(self.unstack_guide_cnn_hist, axis=1))
+
         with tf.variable_scope("role_attention"):
-            inputs = tf.nn.embedding_lookup(self.embedding_matrix, self.input_nl) # [batch_size, self.max_seq_len, self.embedding_dim]
+            inputs = tf.nn.embedding_lookup(self.embedding_matrix, self.predict_nl) # [batch_size, self.max_seq_len, self.embedding_dim]
             lstm_fw_cell = rnn.BasicLSTMCell(self.hidden_size)
             lstm_bw_cell = rnn.BasicLSTMCell(self.hidden_size)
-            _, final_states = tf.nn.bidirectional_dynamic_rnn(lstm_fw_cell, lstm_bw_cell, inputs, sequence_length=self.nl_len, dtype=tf.float32)
+            _, final_states = tf.nn.bidirectional_dynamic_rnn(lstm_fw_cell, lstm_bw_cell, inputs, sequence_length=self.predict_nl_len, dtype=tf.float32)
             final_fw = tf.concat(final_states[0], axis=1)
             final_bw = tf.concat(final_states[1], axis=1)
             cur_rnn_outputs = tf.concat([final_fw, final_bw], axis=1) # concatenate forward and backward final states
             # concat current output with cnn_hist
             outputs = tf.concat([tourist_cnn_hist, guide_cnn_hist, cur_rnn_outputs], axis=1)
             attention = tf.nn.softmax(tf.layers.dense(inputs=outputs, units=2, kernel_initializer=tf.random_normal_initializer, bias_initializer=tf.random_normal_initializer))
-            batch_size = tf.shape(attention)[0]
-            col_0 = tf.concat([tf.expand_dims(tf.range(0, batch_size), axis=1), tf.zeros([batch_size, 1], dtype=tf.int32)], axis=1)
-            col_1 = tf.concat([tf.expand_dims(tf.range(0, batch_size), axis=1), tf.ones([batch_size, 1], dtype=tf.int32)], axis=1)
+            #batch_size = tf.shape(attention)[0]
+            #col_0 = tf.concat([tf.expand_dims(tf.range(0, batch_size), axis=1), tf.zeros([batch_size, 1], dtype=tf.int32)], axis=1)
+            #col_1 = tf.concat([tf.expand_dims(tf.range(0, batch_size), axis=1), tf.ones([batch_size, 1], dtype=tf.int32)], axis=1)
             #role_attention = tf.concat([tf.multiply(tourist_summary, tf.expand_dims(tf.gather_nd(attention, col_0), axis=1)), tf.multiply(guide_summary, tf.expand_dims(tf.gather_nd(attention, col_1), axis=1))], axis=1)
-            role_attention = tf.add(tf.multiply(tourist_rnn_hist, tf.expand_dims(tf.gather_nd(attention, col_0), axis=1)), tf.multiply(guide_rnn_hist, tf.expand_dims(tf.gather_nd(attention, col_1), axis=1)))
+            role_attention = tf.add(tf.multiply(tourist_cnn_hist, tf.expand_dims(tf.unstack(attention, axis=1)[0], axis=1)), tf.multiply(guide_cnn_hist, tf.expand_dims(tf.unstack(attention, axis=1)[1], axis=1)))
             return role_attention
 
     def build_graph(self):
-        tourist_rnn_hist = self.hist_biRNN('tourist')
-        guide_rnn_hist = self.hist_biRNN('guide')
-        if self.use_attention == True:
-            concat_output = self.role_attention(tourist_rnn_hist, guide_rnn_hist)
-        else:
-            concat_output = tf.concat([tourist_rnn_hist, guide_rnn_hist], axis=1)
+        concat_output = self.role_attention()
         history_summary = tf.layers.dense(inputs=concat_output, units=self.intent_dim, kernel_initializer=tf.random_normal_initializer, bias_initializer=tf.random_normal_initializer)
         final_output = self.nl_biRNN(history_summary)
         self.intent_output = tf.layers.dense(inputs=final_output, units=self.intent_dim, kernel_initializer=tf.random_normal_initializer, bias_initializer=tf.random_normal_initializer)
 
     def add_loss(self):
-        self.loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=self.labels, logits=self.intent_output))
-        #self.loss = tf.reduce_mean(tf.nn.weighted_cross_entropy_with_logits(self.labels, self.intent_output, 4))
+        loss_ce = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=self.labels, logits=self.intent_output))
+        stack_tourist_cnn_hist = tf.stack(self.unstack_tourist_cnn_hist, axis=1)
+        stack_guide_cnn_hist = tf.stack(self.unstack_guide_cnn_hist, axis=1)
+        loss_intent_tourist = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=self.tourist_input_intent, logits=stack_tourist_cnn_hist))
+        loss_intent_guide = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=self.guide_input_intent, logits=stack_guide_cnn_hist))
+        self.loss = loss_ce + loss_intent_tourist + loss_intent_guide
         
     def add_train_op(self):
         optimizer = tf.train.AdamOptimizer(learning_rate=1e-3)
